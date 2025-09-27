48410b257e15c917f560bb531a347e440ffd2d98
jamesohara08 <44952453+jamesohara08@users.noreply.github.com>
2025-09-27 02:32:07 -0400

    Added images and new code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const question_list = new Map();
-var manager_choices;
+const manager_choices = [];
 buildQuestionList();
 buildManagerChoices();
 
@@ -49,7 +49,7 @@ function buildQuestionList(){
 	let highest_coach_level = {
 		question: "What is the highest level of coaching you want your next manager to have achieved?",
 		extra_text: "Below are in order of best to worst title by my estimation",
-		answers: ["Manager", "Bench Coach", "Associate Manager", "Hitting/Pitching Coach", "Base Coach", "Position Coach", "Assistant Coach","Any"],
+		answers: ["Manager", "Bench Coach", "Associate Manager", "Hitting/Pitching Coach", "Base Coach", "Position Coach", "Assistant Coach"],
 		next: "age"
 	};
 
@@ -88,7 +88,8 @@ function buildQuestionList(){
 		question: "Should your next manager have made the playoffs as a coach or manager before?",
 		extra_text:"For previous managers, only their managerial record counts here.",
 		answers: ["Yes", "No", "Don't Care"],
-		next_if_yes: "won_pennant"
+		next_if_yes: "won_pennant",
+		next_if_no:"nats_connection"
 	};
 
 	let won_pennant = {
@@ -96,19 +97,13 @@ function buildQuestionList(){
 		extra_text:"For previous managers, only their managerial record counts here.",
 		answers: ["Yes", "No", "Don't Care"],
 		next_if_yes: "won_ws",
-		next_if_no:"manager_bonus"
+		next_if_no:"nats_connection"
 	};
 
 	let won_ws = {
 		question: "Should your next manager have won the World Series as a coach or manager before?",
 		extra_text:"For previous managers, only their managerial record counts here.",
 		answers: ["Yes", "No", "Don't Care"],
-		next: "manager_bonus"
-	};
-
-	let manager_bonus = {
-		question: "Should managers get a bonus multiplier for the playoff questions compared to coaches?",
-		answers: ["Yes, 1.5", "Yes, 2", "Yes, 3", "Yes, 5", "No"],
 		next: "nats_connection"
 	};
 
@@ -130,7 +125,6 @@ function buildQuestionList(){
 	question_list.set("made_playoffs",made_playoffs);
 	question_list.set("won_pennant",won_pennant);
 	question_list.set("won_ws",won_ws);
-	question_list.set("manager_bonus",manager_bonus);
 	question_list.set("top_orgs",top_orgs);
 	question_list.set("player_dev",player_dev);
 	question_list.set("hitting_or_pitching",hitting_or_pitching);
@@ -139,11 +133,12 @@ function buildQuestionList(){
 }
 
 async function buildManagerChoices(){
-	const response = await fetch("https://raw.githubusercontent.com/jamesohara08/NatsManager/refs/heads/master/manager_data.json");
-    manager_choices = await response.json();
-    return data;
+	const response = await fetch("https://raw.githubusercontent.com/jamesohara08/NatsManager/refs/heads/main/manager_data.json?token=GHSAT0AAAAAADMAJHKC23MD5IXSJVIXVGPQ2GXQS5Q");
+    data = await response.json();
+    data.forEach(manager => {
+    	manager_choices.push(manager);
+    });
 }
-
 
 function changeQuestion(question_name){
 	let answers = document.getElementsByName(question_name);
@@ -157,7 +152,7 @@ function changeQuestion(question_name){
 	if(question_name == "nats_connection"){
 		question_template = findManager();
 	} else {
-		let next_question_name = processAnswer(question_name, selected_answer);
+		let next_question_name = processAnswer(question_name, selected_answer, 1);
 		question_template = writeQuestion(next_question_name);
 	}
 	let question = document.getElementById("question");
@@ -167,9 +162,121 @@ function changeQuestion(question_name){
     question.insertAdjacentHTML('beforeend', question_template);
 }
 
-function processAnswer(question_name, answer){
+function processManagerExperience(answer, importance){
+	let threshold = 0;
+	switch(answer) {
+		case "At least 1":
+			threshold = 1;
+		case "At least 3":
+			threshold = 3;
+		case "At least 6":
+			threshold = 6;
+		case "At least 11":
+			threshold = 11;
+		case "15 or more":
+			threshold = 15;
+	}
+	manager_choices.forEach(manager => {
+		if(manager.manager_experience >= threshold){
+			manager.score += importance;
+		}
+	});
+}
+
+function processCoachExperience(answer, importance){
+	let threshold = 0;
+	switch(answer) {
+		case "At least 1":
+			threshold = 1;
+		case "At least 4":
+			threshold = 4;
+		case "At least 6":
+			threshold = 6;
+		case "At least 8":
+			threshold = 8;
+		case "At least 11":
+			threshold = 11;
+		case "15 or more":
+			threshold = 15;
+	}
+	manager_choices.forEach(manager => {
+		if(manager.coach_experience >= threshold){
+			manager.score += importance;
+		}
+	});
+
+}
+
+function processHighestCoachLevel(answer, importance){
+	let acceptable_answers = [];
+	switch(answer){
+		case "Manager":
+			acceptable_answers = ["Manager"];
+		case "Bench Coach":
+			acceptable_answers = ["Manager", "Bench Coach"];
+		case "Associate Manager":
+			acceptable_answers = ["Manager", "Bench Coach", "Associate Manager"];
+		case "Hitting/Pitching Coach":
+			acceptable_answers = ["Manager", "Bench Coach", "Associate Manager", "Hitting/Pitching Coach"];
+		case "Base Coach":
+			acceptable_answers = ["Manager", "Bench Coach", "Associate Manager", "Hitting/Pitching Coach", "Base Coach"];
+		case "Position Coach":
+			acceptable_answers = ["Manager", "Bench Coach", "Associate Manager", "Hitting/Pitching Coach", "Base Coach", "Position Coach"];
+		case "Assistant Coach":
+			acceptable_answers = ["Manager", "Bench Coach", "Associate Manager", "Hitting/Pitching Coach", "Base Coach", "Position Coach", "Assistant Coach"];
+	}
+	manager_choices.forEach(manager => {
+		if(acceptable_answers.includes(manager.highest_coach_level)){
+			manager.score += importance;
+		}
+	});
+}
+
+function processAge(answer, importance){
+	let lower_threshold = 0;
+	let upper_limit = 0;
+	["35-45","46-56","57-68", "68+"]
+	switch(answer) {
+		case "35-45":
+			lower_threshold = 35;
+			upper_limit = 45;
+		case "46-56":
+			lower_threshold = 46;
+			upper_limit = 56;
+		case "57-68":
+			lower_threshold = 57;
+			upper_limit = 68;
+		case "68+":
+			lower_threshold = 68;
+			upper_limit = 120;
+	}
+	manager_choices.forEach(manager => {
+		if(manager.age >= lower_threshold && manager.age <= upper_limit){
+			manager.score += importance;
+		}
+	});
+}
+
+function processAnswer(question_name, answer, importance){
 	let question_info = question_list.get(question_name);
-	if(question_name == "manager_experience" || question_name == "coach_experience" || question_name == 'highest_coach_level' || question_name == "age" || question_name == "hitting_or_pitching" || question_name == "manager_bonus")
+	switch(question_name) {
+		case "manager_experience":
+			processManagerExperience(answer, importance);
+		case "coach_experience":
+			processCoachExperience(answer, importance);
+		case "highest_coach_level":
+			processHighestCoachLevel(answer, importance);
+		case "age":
+			processAge(answer, importance);
+		default:
+			manager_choices.forEach(manager => {
+				console.log(manager[question_name]);
+				if(manager[question_name] == answer){
+					manager.score += importance;
+					console.log(manager.score)
+				}
+			});
+	}
 	var next_question_name;
 	if("next" in question_info){
 		next_question_name = question_info.next;
@@ -203,4 +310,47 @@ function writeQuestion(next_question_name){
 	});
 	question_template +=`<button id="next_question" class="w3-button w3-blue w3-margin" onclick="changeQuestion('${next_question_name}')">Next Question</button>`
 	return question_template;
+}
+
+function findManager(){
+	let highest_score = 0;
+	let winning_managers = [manager_choices[0]];
+	manager_choices.forEach(manager => {
+		if(manager.score > highest_score){
+			winning_managers = [manager];
+		}
+		else if(manager.score == highest_score){
+			winning_managers.push(manager)
+		}
+	});
+	let randomIndex = Math.floor(Math.random() * winning_managers.length);
+	let winning_manager = winning_managers[randomIndex];
+	let manager_name = winning_manager.name;
+	let manager_info = winning_manager.info;
+	let name_array = manager_name.split(" ");
+	let imageURL = name_array[0].toLowerCase() + "_" + name_array[1].toLowerCase() + ".jpg";
+	answer_template = `<h2>Your new Nationals Manager</h2>
+						<img src="images/${imageURL}" class="w3-image"/>
+						<h4>${manager_name}</h4>
+						<p>${manager_info}</p>`
+	if(winning_managers.length > 1){
+		answer_template += `<p>Here are some other equally good choices:</p>
+							<ul class="w3-ul">`
+		winning_managers.forEach(manager => {
+			if(manager.name != manager_name){
+				let runner_up = manager.name;
+				answer_template += `<li>${runner_up}</li>`;
+			}
+		});
+		answer_template += `</ul>`
+	}
+	let currentUrl = window.location.href;
+    let encodedUrl = encodeURIComponent(currentUrl);
+	answer_template += `
+		<div class="w3-bar">
+			<a href="https://bsky.app/intent/compose?text=My next Nationals manager is ${manager_name}! Find yours here: ${encodedUrl}" class="w3-button w3-blue" target="_blank">Share on Bluesky!</a>
+			<a href="https://twitter.com/intent/tweet?text=My next Nationals manager is ${manager_name}! Find yours here: ${encodedUrl}" class="w3-button w3-black" target="_blank">Share on Twitter!</a>
+		</div>
+	`
+	return answer_template;
 }
